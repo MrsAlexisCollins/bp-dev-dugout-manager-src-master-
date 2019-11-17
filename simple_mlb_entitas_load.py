c7217dcf5c4102e5ecd1ebe499eb4997a7f8594e
Harry Pavlidis <harrypav@gmail.com>
2019-11-17 12:50:11 -0600

    this saves data now, locally, using super user

--- simple_mlb_entitas_load.py
+++ simple_mlb_entitas_load.py
@@ -1,21 +1,23 @@
 
 from dugout_manager.mlb_entitas.read import session_read  
 from dugout_manager.mlb_entitas.write import session_write
-from dugout_manager.mlb_entitas.mapping import Bp_people, Bp_xref, Mlb_people
+from dugout_manager.mlb_entitas.mapping import Bp_people, Bp_xref, Mlb_people, Mlb_people_names
 
 from datetime import datetime
 
-mlb_persons = session_read.query(Mlb_people).join(Bp_xref).limit(15)
+mlb_persons = session_read.query(Mlb_people).join(Bp_xref,Mlb_people_names).limit(15)
 
 new_entries = []
 
 for row in mlb_persons:
-   for xref in row.bpxref:
     new_entry = {}
-    new_entry['bpid'] =  xref.bpid
     new_entry['use_full_name'] = row.full_name
     new_entry['updated_timestamp'] = datetime.now()  
-    
+    for xref in row.bpxref:
+        new_entry['bpid'] =  xref.bpid
+    for nameref in row.mlbpeople_names:
+        new_entry['use_sortable_name'] =  nameref.use_name
+
     new_entries.append(new_entry)
 
 for new_entry in new_entries:
