c40a27b952f7afa5ad519ef7a59eefa08b6bac71
Harry Pavlidis <harrypav@gmail.com>
2019-11-17 12:40:49 -0600

    adding another table/rels

--- dugout_manager/mlb_entitas/mapping.py
+++ dugout_manager/mlb_entitas/mapping.py
@@ -18,12 +18,28 @@ class Mlb_people(Base):
     id = Column(Integer, primary_key = True)
     full_name = Column(String)
     bpxref = relationship( "Bp_xref" , back_populates="mlbpeople")
+    mlbpeople_names = relationship("Mlb_people_names", back_populates="mlbpeople")
      
     __table_args__ = {'schema': 'mlbapi'}
 
     def __repr__(self):
         return "\n%s %i %i " % (
           self.full_name ,   self.id  , self.bpxref[0].bpid)
+
+
+
+class Mlb_people_names(Base): 
+    __tablename__ = 'people_names'
+
+    id = Column(Integer, ForeignKey('mlbapi.people.id'), primary_key = True)
+    use_name = Column(String)
+    mlbpeople = relationship( "Mlb_people" , back_populates="mlbpeople_names")
+     
+    __table_args__ = {'schema': 'mlbapi'}
+
+    def __repr__(self):
+        return "\n%s %i %i " % (
+          self.use_name ,   self.id  , self.bpxref[0].bpid)
 
 
 class Bp_xref(Base): 
@@ -48,10 +64,11 @@ class Bp_people(Base):
 
     bpid = Column(Integer, primary_key = True)
     use_full_name = Column(String) 
+    use_sortable_name = Column(String) 
     updated_timestamp = Column(DateTime)
 
     __table_args__ = {'schema': 'entitas'}
 
     def __repr__(self):
         return "a dugout person %s %i   " % (
-          self.use_full_name ,   self.bpid)
+          self.use_full_name ,  self.use_sortable_name ,   self.bpid)
