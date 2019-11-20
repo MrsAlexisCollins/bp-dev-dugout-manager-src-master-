619c9db1a96462f25c80d029372d5121c8199b8c
Harry Pavlidis <harrypav@gmail.com>
2019-11-19 21:45:40 -0600

    roster and status stub

--- mlb_entitas_people_load.py
+++ mlb_entitas_people_load.py
@@ -5,9 +5,9 @@ from dugout_manager.mlb_entitas.mapping import Bp_people, Bp_xref, Mlb_people, M
 
 from datetime import datetime
 
-mlb_persons = session_read.query(Mlb_people).join(Bp_xref,Mlb_people_names).limit(15)
+mlb_persons = session_read.query(Mlb_people).join(Bp_xref,Mlb_people_names)
 
-new_entries = []
+new_entries = [] 
 
 for row in mlb_persons:
     new_entry = {}
@@ -16,10 +16,14 @@ for row in mlb_persons:
     for xref in row.bpxref:
         new_entry['bpid'] =  xref.bpid
     for nameref in row.people_names:
-        new_entry['use_sortable_name'] =  nameref.use_name
+        new_entry['middle_name'] =  nameref.middle_name
+        new_entry['first_name'] =  nameref.use_name
+        new_entry['last_name'] =  nameref.last_name
+        new_entry['use_sortable_name'] =  nameref.last_first_name
+        new_entry['matrilineal_name'] =  nameref.name_matrilineal
 
     new_entries.append(new_entry)
-
+    
 for new_entry in new_entries:
     new_row = Bp_people(**new_entry)
     session_write.add(new_row)
---
+++ mlb_entitas_roster_load.py
@@ -0,0 +1,7 @@
+
+from dugout_manager.mlb_entitas.read import session_read  
+from dugout_manager.mlb_entitas.write import session_write
+from dugout_manager.mlb_entitas.mapping import Bp_xref, Mlb_people_roster_entries, Mlb_people_roster_status, Mlb_player_status_codes, Bp_teams, Bp_people_roster_entries, Bp_people_roster_status
+
+from datetime import datetime
+
