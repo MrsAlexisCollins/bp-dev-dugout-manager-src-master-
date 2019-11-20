408e905d722e1c0144bd3ac8413d973b29d8992e
Harry Pavlidis <harrypav@gmail.com>
2019-11-19 19:46:10 -0600

    now builds teams, run on 'prod' but commented back out now

--- mlb_entitas_team_load.py
+++ mlb_entitas_team_load.py
@@ -10,6 +10,9 @@ bp_organizations = session_write.query(Bp_organizations)
 bp_leagues = session_write.query(Bp_leagues)
 bp_levels = session_write.query(Bp_levels)
 bp_divisions = session_write.query(Bp_divisions)
+mlb_leagues = session_read.query(Mlb_leagues)
+mlb_levels = session_read.query(Mlb_levels)
+mlb_divisions = session_read.query(Mlb_divisions)
 ######### TO DO dupe management
 
 ######### fetch data FROM CAGE
@@ -38,16 +41,32 @@ for team_row in mlb_teams:
     else: 
         new_team_entry['org_id']  = bp_organizations.filter(Bp_organizations.org_name==parent_abbrev.abbreviation).first().org_id
     
-
-
-    new_team_entry['league_id']  =  team_row.league # team_row.league -> Mlb_leagues.id -> Mlb_leagues.abbreviation -> Bp_leagues.league_name -> Bp_leagues.league_id
+    new_team_entry['league_id']  =  bp_leagues.filter(
+        Bp_leagues.league_name == mlb_leagues.filter(Mlb_leagues.id==team_row.league).first().abbreviation 
+        ).first().league_id
     
+    team_level_lookup = mlb_levels.filter(Mlb_levels.id==team_row.level).first()
+    if not team_level_lookup:   
+        new_team_entry['level_id']  =  None
+    else:
+        new_team_entry['level_id']  =   bp_levels.filter(
+            Bp_levels.level_name == team_level_lookup.code 
+        ).first().level_id  
     
-    new_team_entry['level_id']  =   team_row.level  # team_row.level -> Mlb_levels.id -> Mlb_levels.abbreviation -> Bp_levels.level_name -> Bp_levels.level_id
-
-    
-    new_team_entry['division_id']  =   team_row.division #  team_row.division -> Mlb_divisions.id -> Mlb_divisions.abbreviation -> Bp_divisions.division_name -> Bp_divisions.division_id
-
+    team_division_lookup = mlb_divisions.filter(Mlb_divisions.id==team_row.division).first()
+    if not team_division_lookup:   
+        new_team_entry['division_id']  =  None 
+    else:
+        new_team_entry['division_id']  =   bp_divisions.filter(
+            Bp_divisions.division_name == team_division_lookup.abbreviation 
+        ).first().division_id  
 
     new_team_entry['updated_timestamp']  = datetime.now()
-    print (new_team_entry)
+
+    team_entries.append(new_team_entry)
+
+for team_entry in team_entries:
+    new_team_row = Bp_teams(**team_entry)
+    session_write.add(new_team_row)
+
+#session_write.commit()
